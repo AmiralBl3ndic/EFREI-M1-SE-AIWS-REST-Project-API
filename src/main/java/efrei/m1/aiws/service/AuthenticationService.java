de247858a99c178fe5545caf744d00ceca28917d
Camille Briand <camille.briand@efrei.net>
2019-11-19 15:48:25 +0100

    Add AuthenticationService.createJWTToken static method

--- src/main/java/efrei/m1/aiws/service/AuthenticationService.java
+++ src/main/java/efrei/m1/aiws/service/AuthenticationService.java
@@ -2,6 +2,9 @@ package efrei.m1.aiws.service;
 
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.JWT;
+
+import com.auth0.jwt.exceptions.JWTCreationException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,4 +19,24 @@ public class AuthenticationService {
 
 	@Setter
 	private static JWTVerifier jwtVerifier;
+
+	///region JWT Claims
+	private static final String JWT_CLAIM_EMAIL = "email";
+	///endregion
+
+	/**
+	 * Create a valid {@link JWT} token issued by the application
+	 * @param email Email of the user to use
+	 * @return {@link JWT} token with the email in the payload, empty string in case of an error
+	 */
+	public static String createJWTToken(String email) {
+		try {
+			return JWT.create()
+				.withIssuer(jwtIssuer)
+				.withClaim(JWT_CLAIM_EMAIL, email)
+				.sign(jwtAlgorithm);
+		} catch (JWTCreationException e) {
+			return "";
+		}
+	}
 }
