6749d591e736b7bd36e3ffc47a8ce28ccf7fa5ff
Camille Briand <camille.briand@efrei.net>
2019-11-19 14:35:41 +0100

    Add basic JWTInitializer ServletContextListener (useful for CDI)

---
+++ src/main/java/efrei/m1/aiws/config/JWTInitializer.java
@@ -0,0 +1,11 @@
+package efrei.m1.aiws.config;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class JWTInitializer implements ServletContextListener {
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO: Initialize JWT here (loose coupling)
+	}
+}
