fb53b9d17d181d8210384f325f268ee43e7f1d77
Camille Briand <camille.briand@efrei.net>
2019-11-19 17:31:44 +0100

    Add AuthenticationService

---
+++ src/main/java/efrei/m1/aiws/service/AuthenticationService.java
@@ -0,0 +1,9 @@
+package efrei.m1.aiws.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AuthenticationService {
+
+}
