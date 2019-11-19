c17168c143243c9b232c626237a30db9c8692ab7
Camille Briand <camille.briand@efrei.net>
2019-11-19 14:42:58 +0100

    Add efrei.m1.aiws.utils.Constants class

---
+++ src/main/java/efrei/m1/aiws/utils/Constants.java
@@ -0,0 +1,9 @@
+package efrei.m1.aiws.utils;
+
+public class Constants {
+	private Constants() {}
+
+	///region Context Dependency Injection | Attribute Names
+	public static final String CDI_JWT_ALGORITHM = "cdiJwtAlgorithm";
+	///endregion
+}
