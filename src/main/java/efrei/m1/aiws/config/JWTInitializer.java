658ef1eda051066b3e7d6c59d3ff9f57521715a7
Camille Briand <camille.briand@efrei.net>
2019-11-19 15:48:51 +0100

    Rename AuthenticationService to JWTService

--- src/main/java/efrei/m1/aiws/config/JWTInitializer.java
+++ src/main/java/efrei/m1/aiws/config/JWTInitializer.java
@@ -5,7 +5,7 @@ import static efrei.m1.aiws.utils.Constants.*;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import efrei.m1.aiws.service.AuthenticationService;
+import efrei.m1.aiws.service.JWTService;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -21,9 +21,9 @@ public class JWTInitializer implements ServletContextListener {
 		final String issuer = context.getInitParameter(INIT_PARAM_JWT_ISSUER);
 		JWTVerifier verifier = JWT.require(algorithm).withIssuer(issuer).build();
 
-		AuthenticationService.setJwtAlgorithm(algorithm);
-		AuthenticationService.setJwtIssuer(issuer);
-		AuthenticationService.setJwtVerifier(verifier);
+		JWTService.setJwtAlgorithm(algorithm);
+		JWTService.setJwtIssuer(issuer);
+		JWTService.setJwtVerifier(verifier);
 		///endregion
 	}
 }
