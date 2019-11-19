ab67b3f1738d8bf00a8a64e538e9f4c93160ee83
Camille Briand <camille.briand@efrei.net>
2019-11-19 14:57:51 +0100

    Add reference to web.xml context parameters keys in Constants class

--- src/main/java/efrei/m1/aiws/utils/Constants.java
+++ src/main/java/efrei/m1/aiws/utils/Constants.java
@@ -3,6 +3,11 @@ package efrei.m1.aiws.utils;
 public class Constants {
 	private Constants() {}
 
+	///region Init parameters | Names
+	public static final String INIT_PARAM_JWT_ISSUER = "jwtIssuer";
+	public static final String INIT_PARAM_JWT_SECRET = "jwtSecret";
+	///endregion
+
 	///region Context Dependency Injection | Attribute Names
 	public static final String CDI_JWT_ALGORITHM = "cdiJwtAlgorithm";
 	///endregion
