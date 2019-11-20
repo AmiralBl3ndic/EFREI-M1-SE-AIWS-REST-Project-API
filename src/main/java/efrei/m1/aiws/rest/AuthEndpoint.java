6e13f97eab4c93c4eb3450b930c0be0f4222c4a9
Camille Briand <camille.briand@efrei.net>
2019-11-20 18:53:35 +0100

    Update POST /auth endpoint behaviour and simplify its code

--- src/main/java/efrei/m1/aiws/rest/AuthEndpoint.java
+++ src/main/java/efrei/m1/aiws/rest/AuthEndpoint.java
@@ -1,28 +1,42 @@
 package efrei.m1.aiws.rest;
 
 import efrei.m1.aiws.model.requests.JSONAuthPostRequest;
+import efrei.m1.aiws.model.requests.JSONAuthPostResponse;
 import efrei.m1.aiws.service.JWTService;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthEndpoint {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String authenticate(JSONAuthPostRequest body, @Context final HttpServletResponse response) {
-		// TODO: authenticate user from body better than following
+	public Response authenticate(JSONAuthPostRequest body) {
+		JSONAuthPostResponse res = new JSONAuthPostResponse();
+
 		if (body.getEmail().equals("admin@mail.com") && body.getPassword().equals("admin")) {
-			return JWTService.createToken(body.getEmail());
-		} else {
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			return "Wrong email/password";
+			res.setToken(JWTService.createToken(body.getEmail()));
+			return Response.status(Response.Status.OK).entity(res).build();
 		}
+
+		res.setError("Wrong email/password");
+		return Response.status(Response.Status.UNAUTHORIZED).entity(res).build();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response authenticate(@FormParam("email") final String email, @FormParam("password") final String password) {
+		JSONAuthPostResponse res = new JSONAuthPostResponse();
+
+		if (email.equals("admin@mail.com") && password.equals("admin")) {
+			res.setToken(JWTService.createToken(email));
+			return Response.status(Response.Status.OK).entity(res).build();
+		}
+
+		res.setError("Wrong email/password");
+		return Response.status(Response.Status.UNAUTHORIZED).entity(res).build();
 	}
 }
