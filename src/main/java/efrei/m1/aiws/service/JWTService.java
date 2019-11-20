0d9c5b7937b227927f2c9ebe12093b1e1240fcea
Camille Briand <camille.briand@efrei.net>
2019-11-20 16:57:00 +0100

    Add the JWTService.isTokenLegit static method

--- src/main/java/efrei/m1/aiws/service/JWTService.java
+++ src/main/java/efrei/m1/aiws/service/JWTService.java
@@ -1,16 +1,19 @@
 package efrei.m1.aiws.service;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.JWT;
-
 import com.auth0.jwt.exceptions.JWTCreationException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Service handling all operations related to JSON Web Tokens (creation, validation, decoding, ...)s
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JWTService {
+	///region JWT Context Dependency Injection
 	@Setter
 	private static Algorithm jwtAlgorithm;
 
@@ -19,6 +22,7 @@ public class JWTService {
 
 	@Setter
 	private static JWTVerifier jwtVerifier;
+	///endregion
 
 	///region JWT Claims
 	private static final String JWT_CLAIM_EMAIL = "email";
@@ -37,6 +41,21 @@ public class JWTService {
 				.sign(jwtAlgorithm);
 		} catch (JWTCreationException e) {
 			return "";
+		}
+	}
+
+
+	/**
+	 * Verifies whether a {@link JWT} token is legit (hashed with same secret as app)
+	 * @param token String to be considered as a {@link JWT} token
+	 * @return Whether the passed string is a legit token
+	 */
+	public static boolean isTokenLegit(String token) {
+		try {
+			JWTService.jwtVerifier.verify(token);
+			return true;
+		} catch (Exception e) {  // If any exception occurs, the token is considered as not legit
+			return false;
 		}
 	}
 }
