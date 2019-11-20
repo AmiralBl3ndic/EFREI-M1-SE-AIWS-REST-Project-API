339d539a07bfffa20f677ed5e6e528458411bb13
Camille Briand <camille.briand@efrei.net>
2019-11-20 15:19:03 +0100

    Add efrei.m1.aiws.filter.IsAuthenticatedFilter

---
+++ src/main/java/efrei/m1/aiws/filter/IsAuthenticatedFilter.java
@@ -0,0 +1,12 @@
+package efrei.m1.aiws.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+
+public class IsAuthenticatedFilter implements ContainerRequestFilter {
+	@Override
+	public void filter(ContainerRequestContext requestContext) {
+		requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Needs authentication").build());
+	}
+}
