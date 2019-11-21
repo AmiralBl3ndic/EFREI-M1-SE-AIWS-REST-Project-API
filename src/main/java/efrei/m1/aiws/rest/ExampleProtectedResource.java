e1699aa14fc23afe6aef9eeca77223cd43c18914
Camille Briand <camille.briand@efrei.net>
2019-11-21 16:43:42 +0100

    Rename packages to be included in the list of auto-discovered JAX-RS providers

--- src/main/java/efrei/m1/aiws/rest/ExampleProtectedResource.java
+++ src/main/java/efrei/m1/aiws/rest/ExampleProtectedResource.java
@@ -1,13 +1,19 @@
 package efrei.m1.aiws.rest;
 
+import efrei.m1.aiws.rest.filter.annotations.JWTTokenNeeded;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/example/protected")
 public class ExampleProtectedResource {
 	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@JWTTokenNeeded
 	public Response getResource() {
-		return Response.status(Response.Status.OK).build();
+		return Response.ok("Passed").build();
 	}
 }
--- src/main/java/efrei/m1/aiws/rest/filter/JWTTokenNeededFilter.java
+++ src/main/java/efrei/m1/aiws/rest/filter/JWTTokenNeededFilter.java
@@ -1,6 +1,6 @@
-package efrei.m1.aiws.filter;
+package efrei.m1.aiws.rest.filter;
 
-import efrei.m1.aiws.filter.annotations.JWTTokenNeeded;
+import efrei.m1.aiws.rest.filter.annotations.JWTTokenNeeded;
 import efrei.m1.aiws.service.JWTService;
 
 import javax.annotation.Priority;
@@ -12,18 +12,25 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-@Priority(Priorities.AUTHENTICATION)
 @JWTTokenNeeded
+@Priority(Priorities.AUTHENTICATION)
 public class JWTTokenNeededFilter implements ContainerRequestFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext) {
+		Response needsAuthentication = Response.status(Response.Status.UNAUTHORIZED).entity("Needs authentication").build();
+
 		// Gathering JWT Token from Authorization HTTP header
 		String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+		if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer")) {
+			requestContext.abortWith(needsAuthentication);
+			return;
+		}
+
 		String token = authorizationHeader.substring("Bearer".length()).trim();
 
 		// Verify if token is empty or not legit (wrong signature), if so, block the request
 		if (token.isEmpty() || !JWTService.isTokenLegit(token)) {
-			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Needs authentication").build());
+			requestContext.abortWith(needsAuthentication);
 		}
 	}
 }
