c9ce06d021fa5d53e65f70acf19db1b5a28b7443
Camille Briand <camille.briand@efrei.net>
2019-11-22 16:14:01 +0100

    Update User model

--- src/main/java/efrei/m1/aiws/model/User.java
+++ src/main/java/efrei/m1/aiws/model/User.java
@@ -2,9 +2,13 @@ package efrei.m1.aiws.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 @Data @AllArgsConstructor
 public class User {
+    @ToString.Exclude
+    private String dbId;
+
     private String mail;
 
     private String password;
