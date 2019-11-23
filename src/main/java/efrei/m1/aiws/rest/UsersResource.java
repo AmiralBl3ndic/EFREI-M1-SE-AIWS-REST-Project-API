0a0adb269a7c5ba73efc72d80dcb4f54b49d2f0e
Camille Briand <camille.briand@efrei.net>
2019-11-23 23:57:10 +0100

    Do some refactoring for PUT method in UsersResource

--- src/main/java/efrei/m1/aiws/rest/UsersResource.java
+++ src/main/java/efrei/m1/aiws/rest/UsersResource.java
@@ -64,6 +64,56 @@ public class UsersResource {
 		return Response.status(Response.Status.CREATED).entity(res).build();
 	}
 
+	/**
+	 * Processes a PUT request ({@link User} database records update)
+	 * @param authorizationHeader "Authorization" HTTP header (authorization by Bearer token)
+	 * @param userId Path parameter: id of the {@link User} record to update
+	 * @param email Email to set
+	 * @param password Password to set
+	 * @param city City to set
+	 * @return HTTP {@link Response} to send back to the client
+	 */
+	private Response processPUT(String authorizationHeader, String userId, String email, String password, String city) {
+		final String jwtToken = JWTService.extractTokenFromHeader(authorizationHeader);
+		User clientUserRecord = JWTService.getUserFromToken(jwtToken);
+		User userToUpdate = UsersResource.userDAO.findBy(userId);
+
+		// Check if the user record to update exists
+		if (userToUpdate == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+
+		// Check if the client user record exists
+		if (clientUserRecord == null) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+
+		// Check if the user record to update is the one that the requests comes from
+		if (!clientUserRecord.getDbId().equals(userToUpdate.getDbId())) {
+			return Response.status(Response.Status.UNAUTHORIZED).build();
+		}
+
+		// Update user record fields
+		userToUpdate.setEmail(email);
+		userToUpdate.setPassword(AuthenticationService.hashWithBCrypt(password));
+		userToUpdate.setCity(city);
+
+		JSONUsersPostResponse res = new JSONUsersPostResponse();
+		res.setId(userToUpdate.getDbId());
+		res.setToken(JWTService.createToken(userToUpdate.getDbId(), userToUpdate.getEmail()));
+
+		// From this point, we know that the clients have the rights to update a record
+		try {
+			UsersResource.userDAO.update(userToUpdate);
+		} catch (DAOException e) {
+			res.setError(USERS_ERROR_CANNOT_UPDATE);
+			return Response.status(Response.Status.NOT_MODIFIED).entity(res).build();
+		}
+
+		return Response.status(Response.Status.OK).entity(res).build();
+	}
+
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -117,42 +167,6 @@ public class UsersResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response updateUser(JSONUsersPostRequest body, @PathParam("id") String userId, @HeaderParam(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
-		final String jwtToken = JWTService.extractTokenFromHeader(authorizationHeader);
-		User clientUserRecord = JWTService.getUserFromToken(jwtToken);
-		User userToUpdate = UsersResource.userDAO.findBy(userId);
-
-		// Check if the user record to update exists
-		if (userToUpdate == null) {
-			return Response.status(Response.Status.NOT_FOUND).build();
-		}
-
-		// Check if the client user record exists
-		if (clientUserRecord == null) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		}
-
-		// Check if the user record to update is the one that the requests comes from
-		if (!clientUserRecord.getDbId().equals(userToUpdate.getDbId())) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-
-		// Update user record fields
-		userToUpdate.setEmail(body.getEmail());
-		userToUpdate.setPassword(AuthenticationService.hashWithBCrypt(body.getPassword()));
-		userToUpdate.setCity(body.getCity());
-
-		JSONUsersPostResponse res = new JSONUsersPostResponse();
-		res.setId(userToUpdate.getDbId());
-		res.setToken(JWTService.createToken(userToUpdate.getDbId(), userToUpdate.getEmail()));
-
-		// From this point, we know that the clients have the rights to update a record
-		try {
-			UsersResource.userDAO.update(userToUpdate);
-		} catch (DAOException e) {
-			res.setError("Cannot update record");
-			return Response.status(Response.Status.NOT_MODIFIED).entity(res).build();
-		}
-
-		return Response.status(Response.Status.OK).entity(res).build();
+		return this.processPUT(authorizationHeader, userId, body.getEmail(), body.getPassword(), body.getCity());
 	}
 }
