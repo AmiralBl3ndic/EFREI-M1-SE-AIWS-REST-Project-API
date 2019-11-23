6cbb596fb93943e3fed06f543958c74d8e32ecb0
Camille Briand <camille.briand@efrei.net>
2019-11-23 20:01:29 +0100

    Add empty POST requests handling methods to UsersResource

--- src/main/java/efrei/m1/aiws/rest/UsersResource.java
+++ src/main/java/efrei/m1/aiws/rest/UsersResource.java
@@ -1,8 +1,25 @@
 package efrei.m1.aiws.rest;
 
-import javax.ws.rs.Path;
+import efrei.m1.aiws.model.requests.JSONUsersPostRequest;
+import efrei.m1.aiws.model.requests.JSONUsersPostResponse;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("/users")
 public class UsersResource {
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONUsersPostResponse createUser(JSONUsersPostRequest body) {
+		return null;
+	}
 
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONUsersPostResponse createUser(@FormParam("email") String email, @FormParam("password") String password, @FormParam("city") String city) {
+		return null;
+	}
 }
