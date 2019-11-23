58a504e0d0e5818af8466a4cde454cba4c495c30
Camille Briand <camille.briand@efrei.net>
2019-11-23 16:49:54 +0100

    Add documentation + AuthenticationService.hashWithBCrypt static method

--- src/main/java/efrei/m1/aiws/service/AuthenticationService.java
+++ src/main/java/efrei/m1/aiws/service/AuthenticationService.java
@@ -1,4 +1,24 @@
 package efrei.m1.aiws.service;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.security.SecureRandom;
+
+/**
+ * Wrapping class acting as a holder for authentication-related functionalities
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AuthenticationService {
+	private static final int HASH_COST = 12;
+
+	/**
+	 * Hash a string using the BCrypt algorithm
+	 * @param stringToHash String to hash with the BCrypt algorithm
+	 * @return Hashed string with the BCrypt algorithm
+	 */
+	public static String hashWithBCrypt(final String stringToHash) {
+		return BCrypt.with(new SecureRandom()).hashToString(HASH_COST, stringToHash.toCharArray());
+	}
 }
