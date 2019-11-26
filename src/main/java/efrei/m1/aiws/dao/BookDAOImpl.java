0e103bc1db7aa56315e3ea00585f39ec0bdea20c
JulesLagny <jules.lagny@efrei.net>
2019-11-26 14:22:28 +0100

    Creation of the BookDAOImpl.java class

---
+++ src/main/java/efrei/m1/aiws/dao/BookDAOImpl.java
@@ -0,0 +1,29 @@
+package efrei.m1.aiws.dao;
+
+import efrei.m1.aiws.model.Book;
+import lombok.NonNull;
+
+public class BookDAOImpl implements DAO<Book>
+{
+	static final String DB_COL_ID_BOOK = ""
+
+	@Override
+	public void create(@NonNull Book obj) {
+
+	}
+
+	@Override
+	public void update(@NonNull Book obj) {
+
+	}
+
+	@Override
+	public void delete(@NonNull Book obj) {
+
+	}
+
+	@Override
+	public Book findBy(String db) {
+		return null;
+	}
+}
