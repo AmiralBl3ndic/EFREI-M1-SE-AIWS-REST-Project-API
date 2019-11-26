bfdabb01768a7ddd9e7fdd7fecb5e57e90d4ed8c
Camille Briand <camille.briand@efrei.net>
2019-11-26 14:24:56 +0100

    Set a basic GET /video-games endpoint in VideoGamesResource

--- src/main/java/efrei/m1/aiws/rest/VideoGamesResource.java
+++ src/main/java/efrei/m1/aiws/rest/VideoGamesResource.java
@@ -1,4 +1,24 @@
 package efrei.m1.aiws.rest;
 
+import efrei.m1.aiws.dao.VideoGameDAOImpl;
+import lombok.Setter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/video-games")
 public class VideoGamesResource {
+
+	@Setter
+	private static VideoGameDAOImpl videoGameDAO;
+
+	/**
+	 * Get the list of all video-games records in the database
+	 * @return List of all video-games records in the database
+	 */
+	@GET
+	public Response getVideoGames() {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
 }
