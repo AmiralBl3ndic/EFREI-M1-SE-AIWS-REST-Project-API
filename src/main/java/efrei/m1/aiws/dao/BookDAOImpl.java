3b78d4bab58edd70aeea96c0c39d2aaa2dded26f
JulesLagny <jules.lagny@efrei.net>
2019-11-26 16:17:53 +0100

    Add the queries

--- src/main/java/efrei/m1/aiws/dao/BookDAOImpl.java
+++ src/main/java/efrei/m1/aiws/dao/BookDAOImpl.java
@@ -5,6 +5,8 @@ import java.awt.print.Book;
 
 public class BookDAOImpl implements DAO<Book>
 {
+	///region Class Properties
+	///region Database columns names
 	static final String DB_COL_ID_BOOK_ID = "ID_BOOK";
 	static final String DB_COL_USER_ID = "ID_USERS";
 	static final String DB_COL_AUTHOR = "AUTHOR";
@@ -15,6 +17,29 @@ public class BookDAOImpl implements DAO<Book>
 	static final String DB_COL_EDITOR = "EDITOR";
 	static final String DB_COL_AGELIMIT = "AGELIMIT";
 	static final int DB_COL_BOOK_RATING = 0;
+	///endregion
+
+	///region SQL Queries
+	private static final String SQL_SELECT_ALL = "SELECT * FROM BOOKS";
+	private static final String SQL_SELECT_BY_ID_BOOK = "SELECT * FROM BOOKS WHERE ID_BOOK = ?";
+	private static final String SQL_SELECT_BY_ID_USER = "SELECT * FROM BOOKS WHERE ID_USERS = ?";
+	private static final String SQL_SELECT_BY_AUTHOR = "SELECT * FROM BOOKS WHERE AUTHOR = ?";
+	private static final String SQL_SELECT_BY_TITLE = "SELECT * FROM BOOKS WHERE TITLE = ?";
+	private static final String SQL_SELECT_BY_TYPE = "SELECT * FROM BOOKS WHERE TYPE = ?";
+	private static final String SQL_SELECT_BY_RELEASEDATE = "SELECT * FROM BOOKS WHERE RELEASEDATE = ?";
+	private static final String SQL_SELECT_BY_EDITOR = "SELECT * FROM BOOKS WHERE EDITOR = ?";
+	private static final String SQL_SELECT_BY_AGELIMIT = "SELECT * FROM BOOKS WHERE AGELIMIT = ?";
+	private static final String SQL_SELECT_BY_RATING = "SELECT * FROM BOOKS WHERE RATING = ?";
+	///endregion
+
+
+
+
+
+
+
+
+
 
 	@Override
 	public void create(@NonNull Book obj)
