071397286d4a28311bb95cd9ad813a5a35fe647a
Camille Briand <camille.briand@efrei.net>
2019-11-26 14:37:08 +0100

    Add empty basic GET handlers for VideoGameResource

--- src/main/java/efrei/m1/aiws/rest/VideoGamesResource.java
+++ src/main/java/efrei/m1/aiws/rest/VideoGamesResource.java
@@ -3,8 +3,8 @@ package efrei.m1.aiws.rest;
 import efrei.m1.aiws.dao.VideoGameDAOImpl;
 import lombok.Setter;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/video-games")
@@ -13,12 +13,39 @@ public class VideoGamesResource {
 	@Setter
 	private static VideoGameDAOImpl videoGameDAO;
 
+	///region GET requests
 	/**
 	 * Get the list of all video-games records in the database
 	 * @return List of all video-games records in the database
 	 */
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getVideoGames() {
 		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
 	}
+
+	/**
+	 * Get the details of the video-game with database id {@code id}
+	 * @param id Database id of the video-game to get the details of
+	 * @return Details of a specific video-game if it has a record in the database, {@code 404 NOT_FOUND} otherwise
+	 */
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getVideoGameDetails(@PathParam("id") String id) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+
+	/**
+	 * Get the comments of the video-game with database id {@code id}
+	 * @param id Database id of the video-game to get the comments of
+	 * @return List tof comments of a specific video-game record if it has a record in the database, {@code 404 NOT_FOUND} otherwise
+	 */
+	@GET
+	@Path("{id}/comments")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getVideoGameComments(@PathParam("id") String id) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	///endregion
 }
