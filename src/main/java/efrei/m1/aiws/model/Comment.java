13c28c0dff63f2ab702e647afac0f0b712d9ab51
Camille Briand <camille.briand@efrei.net>
2019-11-28 17:13:11 +0100

    Make Comment class generic

--- src/main/java/efrei/m1/aiws/model/Comment.java
+++ src/main/java/efrei/m1/aiws/model/Comment.java
@@ -1,22 +1,17 @@
 package efrei.m1.aiws.model;
 
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data @NoArgsConstructor @AllArgsConstructor
-public class Comment {
+public class Comment<T> {
 
-    private String id_Element;
+    private String content;
 
-    private String id_User;
+    private String creatorId;
 
-    private String comment;
+    private String resourceId;
 
-    //@TODO
-    // Object VideoGame, Book et DVD ?
-
-
+    private T resourceType;
 }
