6eab02d3ec7c7b98627c17713d1faf810a0bb887
Elodie <e.dehache@gmail.com>
2019-12-02 13:25:42 +0100

    constants dvd

--- src/main/java/efrei/m1/aiws/utils/Constants.java
+++ src/main/java/efrei/m1/aiws/utils/Constants.java
@@ -32,5 +32,13 @@ public class Constants {
 	public static final String VIDEOGAMES_ILLEGAL_FILTER_VALUE = "Please specify a valid value for URL parameters";
 	public static final String VIDEOGAMES_ILLEGAL_FILTER_TYPE_INT = "Please specify a valid integer for URL parameter";
 	///endregion
+
+	///region DVD
+	public static final String DVDS_ERROR_NOT_FOUND = "DVD not found";
+	public static final String DVDS_ERROR_NOT_CREATED = "Unable to create dvd";
+	public static final String DVDS_ERROR_NO_CLIENT_ACCOUNT = "Unable to find a valid account to create this resource";
+	public static final String DVDS_ERROR_FORBIDDEN = "You cannot update or delete a record you haven't created";
+	public static final String DVDS_ILLEGAL_FILTER_VALUE = "Please specify a valid value for URL parameters";
+	public static final String DVDS_ILLEGAL_FILTER_TYPE_INT = "Please specify a valid integer for URL parameter";
 	///endregion
 }
